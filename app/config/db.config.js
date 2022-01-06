4b9d88ab4de95ce892e42c9f9be43b482280af03
othmanmtsm <motassimothman@gmail.com>
2022-01-06 16:45:36 +0100

    mongo uri was exposed

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
-    url: "mongodb+srv://admin:admin@cluster0.zf3lu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    url: process.env.MONGO_URL
 };
